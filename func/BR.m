e2f43d0b5bbcc62a067177783848a36f9d95224e
futuresun912 <futuresun912@gmail.com>
2017-05-06 14:38:55 +0900

    v0.3

--- func/BR.m
+++ func/BR.m
@@ -1,27 +1,29 @@
-function [Pre_Labels,Outputs] = BR(train_data,train_target,test_data)
+function [Yt,Conf] = BR(X,Y,Xt)
 %BR Binary Relevance with linear Ridge Regression
 %       Input:
-%           train_data       An N x D data matrix, each row denotes a sample
-%           train_target     An L x N label matrix, each column is a label set
-%           test_data        An Nt x D test data matrix, each row is a test sample
+%           X     An N x D data matrix, each row denotes a sample
+%           Y     An L x N label matrix, each column is a label set
+%           Xt    An Nt x D test data matrix, each row is a test sample
 %       Output:
-%           Pre_Labels       An L x Nt predicted label matrix, each column is a predicted label set
-%           Outputs          An L x Nt output label matrix, each column is a label confidence array
+%           Yt    An L x Nt predicted label matrix, each column is a predicted label set
+%           Conf  An L x Nt output label matrix, each column is a label confidence array
 %
 %  [1] M.R. Boutel et al. Learning multi-label scene classification. Pattern Recognition, 2004.
 
 %% Ridge parameter
 lambda = 0.1;
 
+%% Add bias into the data
+X  = [ones(size(X,1),1),X];
+Xt = [ones(size(Xt,1),1),Xt]; 
+
 %% Ridge Regression
-ww = ridgereg(train_target',train_data,lambda);
-Outputs = [ones(size(test_data,1),1) test_data] * ww;
-Outputs = Outputs';
-Pre_Labels = round(Outputs);
+W    = Y * X / (X'*X+lambda*eye(size(X,2)));
+Conf = W  * Xt';
+Yt   = round(Conf);
 
 %% Regularize the predicted label set
-Pre_Labels(Pre_Labels>1) = 1;
-Pre_Labels(Pre_Labels<1) = 0;
+Yt(Yt>1) = 1;
+Yt(Yt<1) = 0;
    
 end
-
